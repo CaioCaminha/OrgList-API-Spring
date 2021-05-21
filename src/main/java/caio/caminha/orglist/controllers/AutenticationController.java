71fdc5552299a1ca31fb0acefb687aae09388237
caio caminha <caminhacaiopro@gmail.com>
2021-05-21 16:22:25 -0300

    projeto finalizado

--- src/main/java/caio/caminha/orglist/controllers/AutenticationController.java
+++ src/main/java/caio/caminha/orglist/controllers/AutenticationController.java
@@ -1,10 +1,18 @@
 package caio.caminha.orglist.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import caio.caminha.orglist.controllers.Dto.TokenDto;
+import caio.caminha.orglist.controllers.form.LoginForm;
 import caio.caminha.orglist.security.TokenService;
 
 @RestController
@@ -14,4 +22,16 @@ public class AutenticationController {
 	private AuthenticationManager authManager;
 	@Autowired
 	private TokenService tokenService;
+	
+	@PostMapping
+	public ResponseEntity<TokenDto> authenticate(@RequestBody LoginForm loginForm){
+		UsernamePasswordAuthenticationToken dadosLogin = loginForm.convert();
+		try {
+			Authentication authentication = authManager.authenticate(dadosLogin);
+			String token = tokenService.generateToken(authentication);
+			return ResponseEntity.ok(new TokenDto(token, "Bearer"));
+		}catch(AuthenticationException e) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
 }
